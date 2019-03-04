798ad49b8d07fc4e1ea6f825c9504cf91e42092b
Wayne Collier <ke4tri@gmail.com>
2019-03-04 06:33:33 -0600

    Add project files.

---
+++ Lego/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lego
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
